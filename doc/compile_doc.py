4f484c499361ab347d42ce2972d1e3bbe6a58031
Ludovic Charleux <ludovic.charleux@univ-savoie.fr>
2014-09-19 15:36:06 +0200

    Modified doc compiler

--- doc/compile_doc.py
+++ doc/compile_doc.py
@@ -3,9 +3,23 @@
 
 
 if __name__ == '__main__':
-  import os
-  files_in_dir = os.listdir('./')
-  if 'workdir' not in files_in_dir: 
-    os.system('mkdir workdir')
-  os.system('sphinx-build -a . _build/html')  
+  import os, subprocess
+  os.system('git checkout master')
+  proc = subprocess.Popen(["git status"], stdout=subprocess.PIPE, shell=True)
+  (out, err) = proc.communicate()
+  out = out.split('\n')[0]
+  if out == "# On branch master":
+    os.system('sphinx-build -a . _build/html')  
+    os.system('git checkout gh-pages')
+    proc = subprocess.Popen(["git status"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    out = out.split('\n')[0]
+    if out == "# On branch gh-pages":
+      os.system('cp -R _build/html/* ../')
+    else: 
+      print "Commit before compiling doc"
+  else:
+    print "Commit before compiling doc"
+          
+  
   
